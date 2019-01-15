f61a6798b254db1f59a5e8ad9c31695121a97f21
Fyxneo <sasharobert@hotmail.fr>
2019-01-15 15:08:55 +0100

    New CSS

--- public/index.js
+++ public/index.js
@@ -7,9 +7,9 @@
     const div = document.createElement('div');
     const template = actors.map(actor => {
       return `
-        <div class="actor">
+        <div class="actor">	
           <span>${actor.who}</span>
-          <span>${actor.type}</span>
+          <span>${actor.type} :</span>
           <span>${actor.amount}</span>
         </div>
       `;
